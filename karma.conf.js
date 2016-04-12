4aa6289aa09cde3d22c157c610a41af716d06057
Craig Wright <craig.wright@bbc.co.uk>
2016-04-12 12:20:38 +0100

    Added jasmine and karma

---
+++ karma.conf.js
@@ -0,0 +1,41 @@
+module.exports = function(config) {
+  config.set({
+    files: [{
+      pattern: './test/**/*.spec.js',
+      watched: false,
+      included: true,
+      served: true
+    }],
+    preprocessors: {
+      'src/**/*.js': ['webpack'],
+      'test/**/*.js': ['webpack']
+    },
+    browsers: [
+      'Firefox',
+      'Chrome'
+    ],
+    reporters: ['dots', 'coverage'],
+    coverageReporter: {
+      type: 'html',
+      dir: 'test/coverage/'
+    },
+    frameworks: ['jasmine'],
+    webpack: {
+      module: {
+        loaders: [{
+          test: /\.js$/,
+          loader: 'babel',
+          query: {
+            cacheDirectory: true,
+            presets: ['es2015'],
+            plugins: [['__coverage__', { only: 'src/' }]]
+          }
+        }]
+      },
+      watch: true
+    },
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
