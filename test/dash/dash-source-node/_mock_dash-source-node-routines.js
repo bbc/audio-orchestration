dd349baa7230f8d9588b1ebc77ad6ef620622c60
Craig Wright <craig.wright@bbc.co.uk>
2016-04-22 13:57:34 +0100

    Added unit tests for DashSourceNode

---
+++ test/dash/dash-source-node/_mock_dash-source-node-routines.js
@@ -0,0 +1,226 @@
+import MockAudioContext from './../../_mock-audio-context';
+
+import mockAudio from './../../_mock-audio';
+import mockMetadata from './../../_mock-metadata';
+
+const offsetManifest = {
+  type: 'static',
+  minBufferTime: 6,
+  mediaPresentationDuration: 30,
+  maxSegmentDuration: 2,
+  programInformation: {
+    moreInformationURL: 'http://example.org/',
+    lang: undefined,
+    title: 'Example MPD 1',
+    source: null,
+    copyright: null,
+  },
+  baseURL: ['http://example.org/some/mpd'],
+  periods: [{
+    id: 0,
+    duration: 30,
+    start: 2,
+    baseUrl: null,
+    adaptationSets: [{
+      id: '0',
+      mimeType: 'text/json',
+      value: NaN,
+      segmentTemplate: {
+        duration: 98000,
+        timescale: 48000,
+        startNumber: 1,
+        presentationTimeOffset: 0,
+        media: '/md/dash_$Number.json',
+      },
+      representations: [{
+        id: NaN,
+        bandwidth: 129866,
+      }],
+    }, {
+      id: '1',
+      mimeType: 'audio/mp4',
+      value: 5,
+      segmentTemplate: {
+        duration: 98000,
+        timescale: 48000,
+        startNumber: 1,
+        presentationTimeOffset: 0,
+        media: '/1/segment_$Number.m4a',
+      },
+      representations: [{
+        id: 1,
+        bandwidth: 129866,
+      }],
+    }, {
+      id: '2',
+      mimeType: 'audio/mp4',
+      value: 5,
+      segmentTemplate: {
+        duration: 98000,
+        timescale: 48000,
+        startNumber: 1,
+        presentationTimeOffset: 0,
+        media: '/2/segment_$Number.m4a',
+      },
+      representations: [{
+        id: 1,
+        bandwidth: 129866,
+      }],
+    }],
+  }],
+};
+
+const noValidStreamsManifest = {
+  type: 'static',
+  minBufferTime: 16,
+  mediaPresentationDuration: 120,
+  maxSegmentDuration: null,
+  programInformation: {
+    moreInformationURL: 'http://example.org/',
+    lang: undefined,
+    title: 'Example MPD 2',
+    source: null,
+    copyright: null,
+  },
+  baseURL: ['http://example.org/some/mpd'],
+  periods: [{
+    id: 0,
+    duration: 120,
+    start: 0,
+    baseUrl: null,
+    adaptationSets: [{
+      id: '1',
+      mimeType: 'text/text',
+      value: 5,
+      segmentTemplate: {
+        duration: 96000,
+        timescale: 48000,
+        startNumber: 1,
+        presentationTimeOffset: 0,
+        media: '/subtitle/segment_$Number.m4a',
+      },
+      representations: [{
+        id: 1,
+        bandwidth: 129866,
+      }],
+    }],
+  }],
+};
+
+const segmentsUrlPayloadMap = [{
+    url: 'http://example.org/some/mpd/md/dash_1.json',
+    payload: mockMetadata[0],
+  }, {
+    url: 'http://example.org/some/mpd/md/dash_2.json',
+    payload: mockMetadata[1],
+  }, {
+    url: 'http://example.org/some/mpd/md/dash_3.json',
+    payload: mockMetadata[2],
+  }, {
+    url: 'http://example.org/some/mpd/md/dash_4.json',
+    payload: mockMetadata[3],
+  }, {
+    url: 'http://example.org/some/mpd/1/segment_1.m4a',
+    payload: mockAudio[0],
+  }, {
+    url: 'http://example.org/some/mpd/1/segment_2.m4a',
+    payload: mockAudio[1],
+  }, {
+    url: 'http://example.org/some/mpd/1/segment_3.m4a',
+    payload: mockAudio[2],
+  }, {
+    url: 'http://example.org/some/mpd/1/segment_4.m4a',
+    payload: mockAudio[3],
+  }, {
+    url: 'http://example.org/some/mpd/2/segment_1.m4a',
+    payload: mockAudio[3],
+  }, {
+    url: 'http://example.org/some/mpd/2/segment_2.m4a',
+    payload: mockAudio[2],
+  }, {
+    url: 'http://example.org/some/mpd/2/segment_3.m4a',
+    payload: mockAudio[1],
+  }, {
+    url: 'http://example.org/some/mpd/2/segment_4.m4a',
+    payload: mockAudio[0],
+}];
+
+const baseRoutine = {
+  manifest: offsetManifest,
+  segmentsUrlPayloadMap: segmentsUrlPayloadMap,
+  primeParameters: {
+    initial: 1,
+    loop: false,
+    offset: 3,
+    duration: 3,
+  },
+  expected: {
+    channelCount: 10,
+  }
+};
+
+const loopRoutine = {
+  manifest: offsetManifest,
+  segmentsUrlPayloadMap: segmentsUrlPayloadMap,
+  primeParameters: {
+    initial: 2,
+    loop: true,
+    offset: 1,
+    duration: 6,
+    start: 4,
+  },
+  expected: {
+    audioStreams: 2,
+    metadataStreams: 1,
+    channelCount: 10,
+    segments: [{
+        n: 0,
+        number: 1,
+        when: 0,
+        offset: 1,
+        duration: 1,
+        metadata: mockMetadata[0],
+      },{
+        n: 1,
+        number: 2,
+        when: 1,
+        offset: 0,
+        duration: 2,
+        metadata: mockMetadata[1],
+      },{
+        n: 2,
+        number: 3,
+        when: 3,
+        offset: 0,
+        duration: 1,
+        metadata: mockMetadata[2],
+      },{
+        n: 4,
+        number: 1,
+        when: 5,
+        offset: 0,
+        duration: 2,
+        metadata: mockMetadata[0],
+      },{
+        n: 5,
+        number: 2,
+        when: 7,
+        offset: 0,
+        duration: 2,
+        metadata: mockMetadata[1],
+    }],
+  },
+};
+
+const noValidStreamsRoutine = {
+  manifest: noValidStreamsManifest,
+  expected: {
+    channelCount: 0,
+  }
+};
+
+export default [
+  baseRoutine,
+  loopRoutine,
+  noValidStreamsRoutine,
+]
---
+++ test/dash/dash-source-node/dash-source-node.spec.js
@@ -0,0 +1,350 @@
+import DashSourceNode from './../../../src/dash/dash-source-node/dash-source-node';
+import MockAudioContext from './../../_mock-audio-context';
+import mockDashRoutines from './_mock_dash-source-node-routines';
+
+
+describe('DashSourceNode', function() {
+  beforeAll(function () {
+    jasmine.Ajax.install();
+    jasmine.clock().install();
+
+    this.baseRoutine = mockDashRoutines[0];
+    this.loopRoutine = mockDashRoutines[1];
+    this.mockRoutines = mockDashRoutines;
+
+    // Helper function to register responses to segment requests.
+    this.registerSegmentUrls = (segments) => {
+      segments.forEach((segment) => jasmine.Ajax
+        .stubRequest(segment.url)
+        .andReturn({
+          status: 200,
+          contentType: 'application/json',
+          response: segment.payload,
+        })
+      );
+    };
+
+    // Helper function to get a segment from an array by sequence number n.
+    this.getSegment = (segments, n) => {
+      let segment = null;
+      let isFound = false;
+      let i = 0;
+
+      while (!isFound && i < segments.length) {
+        if (segments[i].n === n) {
+          segment = segments[i];
+          isFound = true;
+        }
+        i++;
+      }
+
+      return segment;
+    }
+
+    // The primer offset is required because AudioContext.decodeAudioData cannot
+    // currently decode audio segments without fully formed headers. This will
+    // not be required in a future Web Audio API update.
+    // this._primerOffset = 2048 / this._context.sampleRate;
+    // HACK - to be resolved defaulting sample rate to 48000 to calculate the
+    // primer offset, needs to be resolved.
+    this._primerOffset = 2048 / 48000;
+  });
+
+  afterAll(function() {
+    jasmine.Ajax.uninstall();
+    jasmine.clock().uninstall();
+  });
+
+  it('should construct with correct number of inputs/outputs', function() {
+    this.mockRoutines.forEach((routine) => {
+      const context = new MockAudioContext();
+      const dashSourceNode = new DashSourceNode(context, routine.manifest);
+
+      expect(dashSourceNode.numberOfOutputs).toBe(
+        routine.expected.channelCount);
+    });
+  });
+
+  it('should expose read only presentationDuration', function() {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+
+    expect(dashSourceNode.presentationDuration).toBe(
+      manifest.mediaPresentationDuration);
+    expect(() => {
+      dashSourceNode.presentationDuration = null;
+    }).toThrowError(TypeError);
+  });
+
+  it('should expose read only state', function() {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+
+    expect(dashSourceNode.state).toBe('ready');
+    expect(() => { dashSourceNode.state = null }).toThrowError(TypeError);
+  });
+
+  it('should expose read only and up-to-date playbackTime', function (done) {
+    const context = new MockAudioContext();
+    const routine = this.loopRoutine;
+    const manifest = routine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+
+    expect(dashSourceNode.playbackTime).toBe(0);
+    expect(() => {
+      dashSourceNode.playbackTime = null;
+    }).toThrowError(TypeError);
+
+    this.registerSegmentUrls(routine.segmentsUrlPayloadMap);
+    const {initial, loop, offset, duration} = routine.primeParameters;
+
+    const statechangeCallback = (event) => {
+      if (event.state === 'playing') {
+        // Test initial time.
+        expect(dashSourceNode.playbackTime).toBe(
+          offset + initial);
+
+        // Test after small increment.
+        context.currentTime += 3;
+        expect(dashSourceNode.playbackTime).toBe(
+          offset + (initial + 3) % duration);
+
+        // Test time follows loop.
+        context.currentTime += 10;
+        expect(dashSourceNode.playbackTime).toBe(
+          offset + (initial + 3 + 10) % duration);
+
+        dashSourceNode.stop();
+        done();
+      }
+    }
+
+    dashSourceNode.addEventListener('statechange', statechangeCallback);
+    dashSourceNode.start(initial, loop, offset, duration);
+  });
+
+  it('should emit statechange events', function(done) {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+    this.registerSegmentUrls(this.baseRoutine.segmentsUrlPayloadMap);
+
+    // Ensure that states are emitted and in the corrct order.
+    let statechangeCount = 0;
+    const statechanges = ['priming', 'playing', 'ready'];
+    const statechangeCallback = (event) => {
+      expect(event.state === statechanges[statechangeCount]);
+      statechangeCount++;
+
+      if (event.state === 'playing') {
+        dashSourceNode.stop();
+      } else if (event.state === 'ready') {
+        done();
+      }
+    };
+
+    const {initial, loop, offset, duration} = this.baseRoutine.primeParameters;
+    dashSourceNode.addEventListener('statechange', statechangeCallback);
+    dashSourceNode.start(initial, loop, offset, duration);
+  });
+
+  it('should emit ended event', function (done) {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+    this.registerSegmentUrls(this.baseRoutine.segmentsUrlPayloadMap);
+
+    const statechangeCallback = (event) => {
+      if (event.state === 'playing') {
+        context.currentTime += manifest.mediaPresentationDuration;
+        jasmine.clock().tick(manifest.mediaPresentationDuration * 1000);;
+      }
+    };
+
+    const endedCallback = () => {
+      dashSourceNode.stop();
+      done();
+    };
+
+    const {initial, loop, offset, duration} = this.baseRoutine.primeParameters;
+    dashSourceNode.addEventListener('statechange', statechangeCallback);
+    dashSourceNode.addEventListener('ended', endedCallback);
+    dashSourceNode.start(initial, loop, offset, duration);
+  });
+
+  it('should emit metadata events', function (done) {
+    const routine = this.loopRoutine
+    const expectedSegments = routine.expected.segments;
+    const {initial, loop, offset, duration, start} = routine.primeParameters;
+    this.registerSegmentUrls(routine.segmentsUrlPayloadMap);
+
+    const context = new MockAudioContext();
+    const dashSourceNode = new DashSourceNode(context, routine.manifest);
+
+    let segmentCount = 0;
+    let segmentsToTest = [];
+    const metadataCallback  = (segment) => {
+      // Buffer loaded segments as they may arrive out of sequence order.
+      segmentsToTest.push(segment);
+
+      // Get the current segment expected segment and the corresponding
+      // loaded segment if it exists in the buffer.
+      let mockSegment = expectedSegments[segmentCount];
+      let testSegment = mockSegment ?
+        this.getSegment(segmentsToTest, mockSegment.n) : null;
+
+      // Attempt to advance the expected segment checks as far as possible.
+      while(mockSegment && testSegment) {
+        // Check that segment playback-region metadata is as expected.
+        expect(mockSegment.n).toBe(testSegment.n);
+        expect(mockSegment.when).toBe(testSegment.when);
+        expect(mockSegment.offset).toBe(testSegment.offset);
+        expect(mockSegment.duration).toBe(testSegment.duration);
+        expect(mockSegment.metadata).toBe(testSegment.metadata);
+
+        // If segment was all correct, increment segment number.
+        segmentCount++;
+        if(segmentCount >= expectedSegments.length) {
+          // Stop stream and complete test.
+          dashSourceNode.stop();
+          done();
+        }
+
+        mockSegment = expectedSegments[segmentCount];
+        testSegment = mockSegment ?
+          this.getSegment(segmentsToTest, mockSegment.n) : null;
+      }
+    };
+
+    const statechangeCallback = (event) => {
+      if (event.state === 'playing') {
+        context.currentTime += start;;
+        jasmine.clock().tick(start * 1000);;
+      }
+    };
+
+    dashSourceNode.addEventListener('metadata', metadataCallback);
+    dashSourceNode.addEventListener('statechange', statechangeCallback);
+    dashSourceNode.start(initial, loop, offset, duration);
+  });
+
+  it('should schedule audio correctly', function (done) {
+    const routine = this.loopRoutine
+    const expectedStartParams = routine.expected.segments;
+    const expectedNumberOfSegments = routine.expected.audioStreams *
+      routine.expected.segments.length;
+    const {initial, loop, offset, duration, start} = routine.primeParameters;
+    this.registerSegmentUrls(routine.segmentsUrlPayloadMap);
+
+    const context = new MockAudioContext();
+    const dashSourceNode = new DashSourceNode(context, routine.manifest);
+
+    let segmentCount = 0;
+    context.bufferSourceStartCallback = (when, offset, duration) => {
+      segmentCount++;
+      if(segmentCount >= expectedNumberOfSegments) {
+        // Check that the correct number of segments have been scheduled.
+        expect(context.bufferSourceStartCallback)
+          .toHaveBeenCalledTimes(expectedNumberOfSegments);
+
+        // Check that audio was scheduled for playback as expected.
+        for (let i = 0; i < expectedStartParams.length; i++) {
+          const {when, offset, duration} = expectedStartParams[i];
+          expect(context.bufferSourceStartCallback)
+            .toHaveBeenCalledWith(when, offset + this._primerOffset, duration);
+        }
+
+        dashSourceNode.stop();
+        done();
+      }
+    };
+
+    const statechangeCallback = (event) => {
+      if (event.state === 'playing') {
+        context.currentTime += start;;
+        jasmine.clock().tick(start * 1000);;
+      }
+    };
+
+    spyOn(context, 'bufferSourceStartCallback').and.callThrough();
+    dashSourceNode.addEventListener('statechange', statechangeCallback);
+    dashSourceNode.start(initial, loop, offset, duration);
+  });
+
+  it('should allow start and stop to be called out-of-order', function (done) {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+    this.registerSegmentUrls(this.baseRoutine.segmentsUrlPayloadMap);
+
+    // Should not throw error.
+    dashSourceNode.stop();
+
+    const statechangeCallback = (event) => {
+      if (event.state === 'playing') {
+        // Should not throw error.
+        dashSourceNode.start();
+        dashSourceNode.stop();
+        done();
+      }
+    };
+
+    dashSourceNode.addEventListener('statechange', statechangeCallback);
+    dashSourceNode.start();
+  });
+
+  it('should correctly validate start initial parameter', function () {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+    const {initial, loop, offset, duration} = this.baseRoutine.primeParameters;
+
+    // Test the initial parameter.
+    expect(() => { dashSourceNode.start(- 1); }).toThrowError(Error);
+    expect(() => {
+      dashSourceNode.start(duration, loop, offset, duration);
+    }).toThrowError(Error);
+  });
+
+  it('should correctly validate start loop parameter', function () {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+    const {initial, loop, offset, duration} = this.baseRoutine.primeParameters;
+
+    // Test the loop parameter.
+    expect(() => { dashSourceNode.start(initial, 'str'); }).toThrowError(Error);
+    expect(() => { dashSourceNode.start(initial, 0); }).toThrowError(Error);
+  });
+
+  it('should correctly validate start offset parameter', function () {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+    const {initial, loop, offset, duration} = this.baseRoutine.primeParameters;
+
+    // Test the offset parameter.
+    expect(() => { dashSourceNode.start(0, loop, -1); }).toThrowError(Error);
+    expect(() => {
+      dashSourceNode.start(initial, loop, manifest.mediaPresentationDuration);
+    }).toThrowError(Error);
+  });
+
+  it('should correctly validate start duration parameter', function () {
+    const context = new MockAudioContext();
+    const manifest = this.baseRoutine.manifest;
+    const dashSourceNode = new DashSourceNode(context, manifest);
+    const {initial, loop, offset, duration} = this.baseRoutine.primeParameters;
+
+    // Test the duration parameter.
+    expect(() => {
+      dashSourceNode.start(initial, loop, offset, 0);
+    }).toThrowError(Error);
+    expect(() => {
+      dashSourceNode.start(initial, loop, offset,
+        manifest.mediaPresentationDuration + 1);
+    }).toThrowError(Error);
+  });
+});
