d3592c8b1407d5faa16a1fb002e7400e19829486
Craig Wright <craig.wright@bbc.co.uk>
2016-07-07 09:47:15 +0100

    Changed build target to UMD for easier use with NPM.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: "bbcat.js",
-    libraryTarget: "var",
+    libraryTarget: "umd",
     library: "bbcat"
   },
   module: {
