a9d76fd5255308b6eba364bc051025345c36adbb
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2018-06-27 14:56:57 +0100

    Add an output router to take care of the different handling of stereo and mono-capable devices.

---
+++ src/sequence-renderer/output-router.js
@@ -0,0 +1,75 @@
+/**
+ * @class
+ * @desc The OutputRouter wraps a channel merger node.
+ *
+ * Its inputs (left, right, and mono) correspond to the channelMapping specified in MDO objects.
+ *
+ * It does not perform any gain processing; the mono input is routed unchanged to both the left and
+ * theright output.
+ *
+ * @example
+ * output = new OutputRouter(audioContext, true);
+ * dashSourceNode.outputs[0].connect(output.left);
+ * dashSourceNode.outputs[1].connect(output.right);
+ * dashSourceNode.outputs[2].connect(output.mono);
+ *
+ * output.output.connect(audioContext.destination);
+ */
+class OutputRouter {
+  /**
+   * @param {AudioContext} audioContext
+   * @param {boolean} isStereo whether the output is treated as stereo (true) or mono (false).
+   */
+  constructor(audioContext, isStereo) {
+    this._audioContext = audioContext;
+    this._isStereo = isStereo;
+
+    /**
+     * The left input.
+     * @type {GainNode}
+     */
+    this.left = this._audioContext.createGain();
+
+    /**
+     * The left input.
+     * @type {GainNode}
+     */
+    this.right = this._audioContext.createGain();
+
+    /**
+     * The mono input.
+     * @type {GainNode}
+     */
+    this.mono = this._audioContext.createGain();
+
+    /**
+     * The output.
+     */
+    this.output = this._audioContext.createGain();
+    this.initAudioGraph();
+  }
+
+  /**
+   * Connects the inputs to the output.
+   *
+   * @private
+   */
+  initAudioGraph() {
+    if (this._isStereo) {
+      // Stereo: left and right routed to their respective output channels, mono to both.
+      this._merger = this._audioContext.createChannelMerger(2);
+      this.left.connect(this._merger, 0, 0);
+      this.right.connect(this._merger, 0, 1);
+      this.mono.connect(this.right);
+      this.mono.connect(this.left);
+    } else {
+      // Mono: A single output channel, everything connected to this.
+      this._merger = this._audioContext.createChannelMerger(1);
+      this.mono.connect(this._merger, 0, 0);
+      this.left.connect(this.mono);
+      this.right.connect(this.mono);
+    }
+  }
+}
+
+export default OutputRouter;
--- src/sequence-renderer/sequence-renderer.js
+++ src/sequence-renderer/sequence-renderer.js
@@ -1,18 +1,86 @@
 import Sequence from './sequence';
-
+import OutputRouter from './output-router';
+/**
+ * @class
+ * @desc
+ * The SynchronisedSequenceRenderer is responsible for orchestrating all audio sources related to a
+ * {@link Sequence} on the device it is running on.
+ *
+ *
+ *
+ */
 class SynchronisedSequenceRenderer {
-  constructor(audioContext, clock, sequence) {
+  /**
+   * @param {AudioContext} audioContext
+   * @param {CorrelatedClock} clock
+   * @param {Sequence} sequence
+   */
+  constructor(audioContext, clock, sequence, isStereo) {
+    /**
+     * @type {AudioContext}
+     * @private
+     */
     this._audioContext = audioContext;
+
+    /**
+     * @type {CorrelatedClock}
+     * @private
+     */
     this._clock = clock;
+
+    /**
+     * @type {Sequence}
+     * @private
+     */
     this._sequence = sequence;
+
+    /**
+     * @type {boolean}
+     * @private
+     */
+    this._isStereo = isStereo;
+
+    /**
+     * @type {Array<string>}
+     * @private
+     */
     this._activeObjectIds = [];
+
+    /**
+     * @type {Array}
+     * @private
+     */
     this._activePlaybackItems = {};
 
+    /**
+     * @type {number}
+     * How far in advance are items downloaded and scheduled (seconds).
+     */
     this.lookaheadDuration = 2.0;
+
+    /**
+     * @type {fadeInDuration}
+     * How long does the transition on newly enabled objects last (seconds).
+     */
     this.fadeInDuration = 0.2;
+
+    /**
+     * @type {fadeOutDuration}
+     * How long does the transition on newly removed objects last (seconds).
+     */
     this.fadeOutDuration = 0.2;
 
+
+    /**
+     * @type {RendererOutputRouter}
+     * @private
+     */
+    this._output = new OutputRouter(this._isStereo);
+
+    // listen for changes to the master clock object
     this._clock.on('update', this.notify.bind(this));
+
+    // set up the routing based on the sequence description
     this.initAudioGraph();
   }
 
@@ -54,6 +122,17 @@ class SynchronisedSequenceRenderer {
   }
 
   /**
+   * Get an output gain node that can be connected to any further processing.
+   *
+   * It has a single output, that can have either one or two channels.
+   *
+   * @returns {GainNode}
+   */
+  get output() {
+    return this._output.output;
+  }
+
+  /**
    * Initialises the audio graph based on the sequence description.
    */
 
@@ -73,6 +152,8 @@ class SynchronisedSequenceRenderer {
    *
    * Add it to the list of active objects, and the next {@link notify} call will create the players
    * and schedule the fade-in once the player has started playing.
+   *
+   * @param {string} objectId
    * @private
    */
   addObject(objectId) {
@@ -85,6 +166,9 @@ class SynchronisedSequenceRenderer {
    * Handle removal of an object.
    *
    * If any players are associated with this object, they will fade out and then be destroyed.
+   *
+   * @param {string} objectId
+   * @private
    */
   removeObject(objectId) {
     this._activeObjectIds.remove(objectId);
