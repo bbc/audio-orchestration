8554b8e9cd21fdebc6e1e6e252bd7110d26290ab
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2018-07-04 09:32:47 +0100

    Add empty react project for mdo-allocation example

---
+++ examples/mdo-allocation/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const App = () => (
+  <p>Hello</p>
+);
+
+export default hot(module)(App);
---
+++ examples/mdo-allocation/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, 'sequence.json') },
+    ]),
+  ],
+};
