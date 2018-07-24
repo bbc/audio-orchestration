a01dcf02f2e95e70c68948071978a5cd736fddfd
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2018-07-24 09:58:03 +0100

    Initial implementation with single session wide sync clock.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 /* eslint-disable import/prefer-default-export */
-export const SESSION_CODE_LENGTH = 6;
+export const SESSION_CODE_LENGTH = 4;
 export const TIMELINE_TYPE = 'tag:rd.bbc.co.uk,2015-12-08:dvb:css:timeline:simple-elapsed-time:1000';
+export const TIMELINE_TYPE_TICK_RATE = 1000;
 export const CLOUDSYNC_ENDPOINT = 'mqttbroker.edge.platform.2immerse.eu';
+export const SEQUENCE_URLS = [
+  'audio/clicks/sequence.json',
+];
+export const CONTENT_ID = 'github.com/bbc/bbcat-orchestration-template/syncClock';
--- src/template/actions/orchestration.js
+++ src/template/actions/orchestration.js
@@ -1,5 +1,4 @@
 import uuidv4 from 'uuid/v4';
-// This is the entrance to the rabbit hole.
 
 import Clocks from 'dvbcss-clocks';
 import SyncPlayers from 'bbcat-orchestration/src/sync-players';
@@ -8,7 +7,14 @@ import MdoAllocation from 'bbcat-orchestration/src/mdo-allocation';
 import CloudSyncAdapter from 'bbcat-orchestration/src/sync/cloud-sync-adapter';
 import Sync from 'bbcat-orchestration/src/sync/sync';
 
-import { CLOUDSYNC_ENDPOINT, TIMELINE_TYPE } from '../../config';
+import {
+  CLOUDSYNC_ENDPOINT,
+  TIMELINE_TYPE,
+  TIMELINE_TYPE_TICK_RATE,
+  SEQUENCE_URLS,
+  SESSION_CODE_LENGTH,
+  CONTENT_ID,
+} from '../../config';
 
 const { AudioContextClock } = SyncPlayers;
 const { Sequence, SynchronisedSequenceRenderer } = SequenceRenderer;
@@ -36,6 +42,18 @@ const setError = errorMessage => ({
   errorMessage,
 });
 
+const setPrimaryObject = objectId => ({
+  type: 'SET_PRIMARY_OBJECT',
+  objectId,
+});
+
+const selectPrimaryObject = (contentId, activeObjectIds) => (dispatch) => {
+  console.warn('selectPrimaryObject not implemented');
+  // using some list and the orchestration state's list of running sequences,
+  // pick an object id that has a picture associated with it (based on some priority?)
+  dispatch(setPrimaryObject(activeObjectIds[0]));
+};
+
 // Shim for safari
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
@@ -44,19 +62,22 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const orchestrationState = {
   initialised: false,
   sequences: [],
+  masterClock: null,
+  syncClock: null,
   renderers: {},
-  activeSequenceId: null,
+  activeSequenceUrl: null,
   audioContext: null,
   sysClock: null,
   sync: null,
   deviceId: null,
+  master: false,
 };
 
 const requestSessionId = (userSessionCode) => {
   let sessionCode = userSessionCode;
   if (userSessionCode === undefined) {
     console.warn('Generating random session id, not guaranteed to be unique.');
-    sessionCode = [...Array(6).keys()]
+    sessionCode = [...Array(SESSION_CODE_LENGTH).keys()]
       .map(() => `${Math.floor(Math.random() * 10)}`)
       .join('');
   }
@@ -69,62 +90,86 @@ const requestSessionId = (userSessionCode) => {
 
 const generateDeviceId = () => `bbcat-orchestration-device-${uuidv4()}`;
 
-const loadSequence = url => fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`Could not download sequence data from ${url} (${response.status}).`);
-    }
-    return response.json();
-  })
-  .then(data => new Sequence(data))
-  .then(sequence => orchestrationState.sequences.push(sequence));
+const loadSequence = (url) => {
+  console.debug('loadSequence', url);
+  const {
+    sequences,
+    audioContext,
+    master,
+    syncClock,
+  } = orchestrationState;
 
-const startSequence = (sequenceId) => {
-  const sequence = orchestrationState.sequences.find(s => s.id === sequenceId);
-  if (sequence === undefined) {
-    throw new Error(`Sequence '${sequenceId}' not found.`);
-  }
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Could not download sequence data from ${url} (${response.status}).`);
+      }
+      return response.json();
+    })
+    .then(data => new Sequence(data))
+    .then((sequence) => {
+      // create a renderer but don't start it yet.
+      const renderer = new SynchronisedSequenceRenderer(
+        audioContext,
+        syncClock,
+        sequence,
+        master, // isStereo
+      );
+
+      // connect renderer to output
+      renderer.output.connect(audioContext.destination);
+
+      // add the sequence, its url, and its renderer, to the list of available sequences.
+      sequences.push({
+        url,
+        sequence,
+        renderer,
+      });
+    });
 };
 
 export const initialiseOrchestration = (master, {
   joinSessionCode = undefined,
-  sequenceUrls = [],
-  initialSequenceId,
 } = {}) => (dispatch) => {
   if (orchestrationState.initialised === true) {
     throw new Error('Orchestration system has already been initialised.');
   }
   orchestrationState.initialised = true;
+
   dispatch(setLoading(true));
+  dispatch(setRole(master ? 'master' : 'slave'));
 
   const audioContext = new AudioContext();
   audioContext.resume();
   const sysClock = new AudioContextClock({}, audioContext);
   const sync = new Sync(new CloudSyncAdapter({ sysClock }));
+  const masterClock = new CorrelatedClock(sync.wallClock, {
+    correlation: {
+      parentTime: wallClock.now(),
+      childTime: 0,
+    },
+    speed: 0,
+    tickRate: TIMELINE_TYPE_TICK_RATE,
+  });
   const deviceId = generateDeviceId();
 
   Object.assign(orchestrationState, {
     master,
     audioContext,
     sysClock,
+    masterClock,
     sync,
     deviceId,
-    sequenceUrls,
   });
 
-  // 1. Download all the sequence descriptions - TODO: should be separate.
-  // 2. Request/generate a full session id
-  // 3. Store the session ID and connect to cloud sync service
-  // 4. Create an MDO Helper for managing object allocations.
-  // 5. If an initial sequence has been specified, create its renderer.
+  // 1. Request/generate a full session id
+  // 2. Store the session ID and connect to cloud sync service
+  // 3. Create an MDO Helper for managing object allocations.
+  // 4. Download all the sequence descriptions and initialise renderers
+  // 5. If an initial sequence has been specified, start playing it.
   // 6. Dispatch actions changing the user interface to indicate the connection is ready.
 
-  Promise.all(sequenceUrls.map(loadSequence))
-    .then(() => requestSessionId(joinSessionCode))
-    .catch((e) => {
-      dispatch(setError(`Could not join session ${joinSessionCode}.`));
-      throw e;
-    })
+  orchestrationState.initPromise = requestSessionId(joinSessionCode)
     .then(({ sessionCode, sessionId }) => {
       orchestrationState.sessionCode = sessionCode;
       dispatch(setSessionCode(sessionCode));
@@ -132,31 +177,57 @@ export const initialiseOrchestration = (master, {
     })
     .then(() => {
       if (master) {
+        sync.provideTimelineClock(masterClock, TIMELINE_TYPE, CONTENT_ID);
+      }
+
+      return sync.requestTimelineClock(TIMELINE_TYPE, CONTENT_ID);
+    })
+    .then((timelineClock) => {
+      orchestrationState.syncClock = timelineClock;
+    })
+    .catch((e) => {
+      dispatch(setError(`Could not ${master ? 'create' : 'join'} session ${joinSessionCode}.`));
+      throw e;
+    })
+    .then(() => Promise.all(SEQUENCE_URLS.map(loadSequence)))
+    .catch((e) => {
+      dispatch(setError('Downloading sequences failed.'));
+      throw e;
+    })
+    .then((sequences) => {
+      if (master) {
         orchestrationState.mdoHelper = new MdoAllocator(deviceId);
       } else {
         orchestrationState.mdoHelper = new MdoReceiver(deviceId);
       }
+      orchestrationState.mdoHelper.on('change', ({ contentId, activeObjects }) => {
+        dispatch(selectPrimaryObject(contentId, activeObjects));
+        sequences.forEach(({ url, renderer }) => {
+          if (url === contentId) {
+            renderer.setActiveObjectIds(activeObjects);
+          }
+        });
+      });
+      orchestrationState.mdoHelper.on('schedule', (schedule) => {
+        dispatch(scheduleSequences(schedule));
+      });
       orchestrationState.mdoHelper.start(sync);
     })
     .then(() => {
-      const sequence = orchestrationState.sequences.find(s => s.id === initialSequenceId);
-      if (sequence !== undefined) {
-        dispatch(startSequence(sequence.id));
-      }
-    })
-    .then(() => {
-      dispatch(setRole(master ? 'master' : 'slave'));
+      console.debug('loaded all sequences');
       dispatch(setLoading(false));
       sync.on('SyncServiceUnavailable', () => {
         console.error('SyncServiceUnavailable');
-        setError('Lost connection to synchronisation service');
+        dispatch(setError('Lost connection to synchronisation service.'));
       });
     })
     .catch((e) => {
       console.error('initialiseOrchestration', e);
       throw e;
     });
+  return orchestrationState.initPromise;
 };
+
 
 export const play = () => (dispatch) => {
 
