764c49d99e9759b56d6536dcd47bf52bcb88c78c
Jon Francombe <jon.francombe@bbc.co.uk>
2020-05-05 12:11:32 +0100

    Tidy some template display issues (#64)
    * Make control display box the right size
    
    * Don't render PageOverflow for controls if it's empty
    
    * Only render choices on main devices
    
    * Simpler fix for hiding choices scrollbar on main and aux
    
    * Tidy PageOverflow css, increment version number

--- src/pages/playing-page/PlayingPage.jsx
+++ src/pages/playing-page/PlayingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import config from 'config';
@@ -21,7 +21,10 @@ import {
 // TODO putting this here to ensure it is in click event, should probably be in sagas.js instead.
 import { ensureAudioContext } from '../../template/orchestration';
 
-const PlayingPage = () => (
+const PlayingPage = ({
+  // Get activeControlIds from state
+  activeControlIds,
+}) => (
   <div className={classnames('page', 'page-playing', 'page-with-status-bar')}>
     <ConnectedStatusBar instructions />
 
@@ -33,16 +36,32 @@ const PlayingPage = () => (
       <ConnectedChoices />
 
       { config.DEBUG_UI ? <ConnectedObjectList /> : null }
-    </PageContents>
 
-    <PageOverflow>
-      <ConnectedControls controls={config.CONTROLS} />
-    </PageOverflow>
+      { activeControlIds.length > 0
+        ? (
+          <PageOverflow>
+            <ConnectedControls controls={config.CONTROLS} />
+          </PageOverflow>
+        ) : null }
+
+    </PageContents>
   </div>
 );
 
-// TODO may not need any state on start page.
-const mapStateToProps = () => ({});
+
+PlayingPage.propTypes = {
+  activeControlIds: PropTypes.arrayOf(
+    PropTypes.string,
+  ).isRequired,
+};
+
+
+const mapStateToProps = ({
+  // state
+  activeControlIds,
+}) => ({
+  activeControlIds,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   onClickStart: () => {
