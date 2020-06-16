18a62b5336f02dffbcfd1285bd0d4bd18745eda6
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2020-06-16 09:49:06 +0100

    Implement StereoPanner for mono sources in browsers that do not support it natively (#65)
    * Implement StereoPanner for mono sources manually if not supported. Not supporting stereo sources because the library in general does not, either.
    
    * bump version

--- src/sequence-renderer/output-router.js
+++ src/sequence-renderer/output-router.js
@@ -44,9 +44,41 @@ class OutputRouter {
    * @private
    */
   initAudioGraph() {
-    const panner = this._audioContext.createStereoPanner();
-    panner.pan.value = this._panning;
-    this.input.connect(panner);
+    let panner;
+
+    if (this._audioContext.createStereoPanner) {
+      // If available, use a simple StereoPannerNode
+      panner = this._audioContext.createStereoPanner();
+      panner.pan.value = this._panning;
+      this.input.connect(panner);
+    } else {
+      // TODO this manual implementation of panning algorithm only works for mono sources!
+      // Spec: https://webaudio.github.io/web-audio-api/#stereopanner-algorithm
+
+      // Steps 1-3: clamp to [-1, 1] and then normalise to [0, 1]
+      const x = (Math.min(Math.max(-1, this._panning), 1) + 1) / 2;
+
+      // Create gain nodes
+      const gainLeft = this._audioContext.createGain();
+      const gainRight = this._audioContext.createGain();
+
+      // Step 4: calculate gain values
+
+      gainLeft.gain.value = Math.cos(x * (Math.PI / 2));
+      gainRight.gain.value = Math.sin(x * (Math.PI / 2));
+
+      // Step 5: connect mono input to both gain nodes
+      this.input.connect(gainLeft);
+      this.input.connect(gainRight);
+
+      // Create and connect a channel merger for combining left and right channels to stereo output
+      const merger = this._audioContext.createChannelMerger();
+      gainLeft.connect(merger, 0, 0);
+      gainRight.connect(merger, 0, 1);
+
+      // Assign merger to panner so its output can be used below.
+      panner = merger;
+    }
 
     if (this._isStereo) {
       panner.connect(this.output);
