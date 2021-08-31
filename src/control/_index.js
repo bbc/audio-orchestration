e6c053060c5e55f22c46fc46695624e5a4e40c91
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2021-08-31 17:06:02 +0100

    Remove unused examples, demos, controls source files. (#7)

--- src/control/_index.js
+++
@@ -1,7 +0,0 @@
-import OrientationControls from './orientation-controls.js';
-import GLOrientationControls from './gl-orientation-controls.js';
-
-export {
-  OrientationControls,
-  GLOrientationControls,
-};
