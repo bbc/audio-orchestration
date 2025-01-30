e942bf685cead3806dabfa817b6440f956275f9c
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2025-01-30 11:52:37 +0000

    Remove makerbox links and update links to AO documentation (#69)
    * Remove makerbox links and update links to AO documentation
    
    * Remove BBC Logo from default template footer
    
    * bump version
    
    * spelling

--- packages/template/src/components/footer/Footer.jsx
+++ packages/template/src/components/footer/Footer.jsx
@@ -3,18 +3,11 @@
  * This source code is licensed under the GPL license found in the LICENSE file in this repository.
  */
 import React from 'react';
-import Icon from 'components/icon/Icon';
 
 const Footer = () => (
   <div className="footer">
-    <p className="logo">
-      <Icon title="BBC Research & Development" padded name="bbcrd" />
-    </p>
     <p className="small">
-      <a href="https://www.bbc.co.uk/makerbox/tools/audio-orchestrator" target="_blank" rel="noopener noreferrer">Created with Audio Orchestrator</a>
-    </p>
-    <p className="small">
-      &copy; BBC R&amp;D 2022
+      <a href="https://bbc.github.io/audio-orchestrator" target="_blank" rel="noopener noreferrer">Audio Orchestrator</a>
     </p>
   </div>
 );
